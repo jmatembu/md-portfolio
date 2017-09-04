f2fe1b1972d24c912a096fab0eb155c3255dd38a
Joseph Matembu <jmatembu@gmail.com>
2017-09-05 00:57:28 +0300

    Add greeting for new visitors

--- js/app.js
+++ js/app.js
@@ -2,6 +2,21 @@
 	'use strict';
 
 	var getFactBtn = document.getElementById('get-fact');
+	var isGreeted = /^(.*;)?\s*greeted\s*=\s*[^;]/.test(document.cookie);
+	var greeting = document.querySelector('.greeting');
+
+	if ( ! isGreeted ) {
+		
+		setTimeout(function () {
+			greeting.classList.add('reveal');
+		}, 5000);
+
+		setTimeout(function () {
+			greeting.classList.remove('reveal');
+		}, 15000);
+
+		document.cookie = 'greeted=true;max-age=31556952000';
+	}
 
 	window.addEventListener('DOMContentLoaded', getRandomNumberFact);
 	getFactBtn.addEventListener('click', getRandomNumberFact);
@@ -15,6 +30,8 @@
 		req.onload = function () {
 			if ( req.status === 200 && req.status < 400 ) {
 				fact.innerHTML = req.responseText;
+			} else {
+				fact.innerHTML = 'Error: Could not load fact.';
 			}
 		}
 
